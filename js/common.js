c029d35c4a1a4cc8a9c89ebfb5e8f5ad618f9137
Vijay-yeruva <Vijayasekhar.yeruva@gmail.com>
2021-11-24 09:54:50 -0500

    Revert "updated research areas"
    This reverts commit 116dd610211364e032205c8910506baa5c8760ec.

--- js/common.js
+++ js/common.js
@@ -16,6 +16,7 @@ let addTopNav = function (page, markactive = true, extraindirection = false) {
     toggler.setAttribute("aria-label", "Toggle navigation");
     let span = document.createElement("span");
     span.classList.add("navbar-toggler-icon");
+    toggler.appendChild(span);
     topnav.appendChild(toggler);
 
     let navcontent = document.createElement("div");
@@ -50,18 +51,17 @@ let addTopNav = function (page, markactive = true, extraindirection = false) {
         navItemlink.innerText =  item.item;
 
         if (item.hasOwnProperty('subItems')){
-            navItemlink.classList.add("dropdown");
+            navItem.classList.add("dropdown");
             navItemlink.classList.add("dropdown-toggle");
             navItemlink.setAttribute("id","navbarDropdown");
-            navItemlink.setAttribute("role","button");
             navItemlink.setAttribute("data-toggle","dropdown");
             navItemlink.setAttribute("aria-haspopup","true");
             navItemlink.setAttribute("aria-expanded","false");
             navItemlink.onclick = function(){
                 location.href = this.getAttribute("href");
+                this.nextSibling.classList.remove("show");
             };
             navItem.appendChild(navItemlink);
-
             let dropdowndiv = document.createElement("div");
             dropdowndiv.classList.add("dropdown-menu")
             dropdowndiv.setAttribute("aria-labelledby","navbarDropdown");
@@ -439,3 +439,4 @@ let formatPara = function(text){
     }
     return result;
 }
+

eadaacae5f7119318477941b7dad67fd8436d35e
Yeruva <vyeruva@albany.edu>
2021-11-29 09:12:30 -0500

    added projects pages

---
+++ projects/js/project3.js
@@ -0,0 +1,51 @@
+let requestURL = "../data/project3.json";
+let request = new XMLHttpRequest();
+//getting content Element to append grants information
+let maincontentContainer = document.getElementsByClassName('main-content')[0];
+request.open('GET', requestURL);
+request.responseType = 'json';
+request.send();
+request.onload = function () {
+    let content = '';
+    const datajson = request.response;
+    //condition for checking if browser is Internet Explorer
+    let data = ((false || !!document.documentMode)) ? JSON.parse(datajson) : datajson;
+    let contentElement = document.createElement('div');
+    contentElement.classList.add('content');
+    contentElement.innerHTML = getContent(data.webelements);
+    maincontentContainer.appendChild(contentElement);
+    contentElement = document.createElement('div');
+    contentElement.classList.add('project-team');
+    contentElement.innerHTML = buildteam(data.members);
+    maincontentContainer.appendChild(contentElement);
+    addfooter();
+}
+
+let buildteam = function(members){
+    content ="";
+    for (let i = 0; i < members.length; i++) {
+        let member = members[i];
+        content += '<div class = "col-lg-3 col-md-4 col-sm-6" style="height: 500px;">'+
+                    '<a href = "../speakers/'+ member.link+'">'+
+                    '<p class = "speakers-info">'+
+                    '<img class = "speakers-img" style="height: 200.887px; width: 200.887px; border-radius: 50%;" src= "assets/images/Speakers/'+ member.photo+'" />'+
+                    '<br>'+
+                    '<span class = "title">'+
+                    '<strong>'+member.name+'</strong>'+
+                    '<br>'+
+                    '<span style="font-size: 16px;">'+
+                    member.university+
+                    '</span>'+
+                    '</span>'+
+                    '</p>'+
+                    '</a>'+
+                    '</div>';
+    }
+    return content;
+}
+
+$('.carousel').carousel({
+    pause: "false",
+    interval: 2000
+
+});

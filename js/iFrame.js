77501e40cf485d18668e85c3c526214b54868259
Sahil Ghauri <sghauri@albany.edu>
2022-01-26 14:58:18 -0500

    fixed iFrame width issue

--- js/iFrame.js
+++ js/iFrame.js
@@ -1,19 +1,21 @@
 // set initial size
 $(document).ready(function() {
-    SetIframeSize();
-  });
+  SetIframeSize();
+});
+
+// resize on window resize
+$(window).on('resize', function() {
+  SetIframeSize();
+});
   
-  // resize on window resize
-  $(window).on('resize', function() {
-    SetIframeSize();
-  });
-  
-  function SetIframeSize() {
-    if($("#external").width($(window).width()) <= 600 ){
-        $("#external").width($(window).width() - 50);
-    }
-    else{
-        $("#external").width($(window).width() * 0.85);
-    }
-    //$("#external").height($(window).height() - 200);
+function SetIframeSize() {
+  if($(window).width() <= 600 ){
+      $("#external").width($(window).width() - 50);
+      // console.log("q1");
+  }
+  else{
+      $("#external").width($(window).width() * 0.85);
+      // console.log($(window).width());
+  }
+  $("#external").height($(window).height() - 200);
 }

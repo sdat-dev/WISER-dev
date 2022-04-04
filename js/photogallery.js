d20cfbf20559eef30ae0cca2fdf3d8c81ed55404
Sahil Ghauri <sghauri@albany.edu>
2022-04-04 16:22:56 -0400

    added gallery

--- js/photogallery.js
+++ js/photogallery.js
@@ -1,21 +1,57 @@
 let filename = "photogallery";
-let requestURL = "data/photogallery.json";
-let request = new XMLHttpRequest();
-//getting content Element to append grants information
-let maincontentContainer = document.getElementsByClassName('main-content')[0];
-request.open('GET', requestURL);
-request.responseType = 'json';
-request.send();
-request.onload = function () {
+window.onload = function () {
+    let requestURL = "data/photogallery.json";
+    let imageDirMusiciansURL = "data/imageDirMusicians.json";
+    let imageDirWISERindustryURL = "data/imageDirWISERindustry.json";
+    let imageDirWISERpresentationsURL = "data/imageDirWISERpresentations.json";
+    let imageDirWISERteamURL = "data/imageDirWISERteam.json";
+    let request = axios.get(requestURL);
+    let Musicians = axios.get(imageDirMusiciansURL);
+    let WISERindustry = axios.get(imageDirWISERindustryURL);
+    let WISERpresentations = axios.get(imageDirWISERpresentationsURL);
+    let WISERteam = axios.get(imageDirWISERteamURL);
+    let maincontentContainer = document.getElementsByClassName('main-content')[0];
+
+    axios.all([request, Musicians, WISERindustry, WISERpresentations, WISERteam]).then(axios.spread((...responses) => {
+        let web = responses[0].data;
+        let Musicians = responses[1].data;
+        let WISERindustry = responses[2].data;
+        let WISERpresentations = responses[3].data;
+        let WISERteam = responses[4].data;
+        let webelements = web;
+        let content = getContent(webelements);
+        content += '<div class="row">' +
+                '<div class="col-lg-4 col-md-12 mb-4 mb-lg-0"' +
+                'style="'+
+                'display: grid;'+
+                'grid-template-columns: repeat(3, 5fr);'+
+                'gap: 10px;'+
+                'min-width: fit-content;>' + 
+                buildGallery(Musicians) + 
+                buildGallery(WISERindustry) + 
+                buildGallery(WISERpresentations) + 
+                buildGallery(WISERteam) + 
+                '</div>';
+        let contentElement = document.createElement('div');
+        contentElement.classList.add('content');
+        contentElement.innerHTML = content.trim();
+        maincontentContainer.appendChild(contentElement);
+        addfooter();
+    })).catch(errors => {
+        console.log(errors);
+    })
+}
+
+let buildGallery = function(images){
     let content = '';
-    const webelementsjson = request.response;
-    //condition for checking if browser is Internet Explorer
-    let webelements = ((false || !!document.documentMode)) ? JSON.parse(webelementsjson) : webelementsjson;
-    let contentElement = document.createElement('div');
-    contentElement.classList.add('content');
-    contentElement.innerHTML = getContent(webelements,filename);
-    maincontentContainer.appendChild(contentElement);
-    addfooter();
+    images.forEach((image) => {
+        content+='<img '+
+        'src="' + image.URL + '"' +
+        'class="w-100 shadow-1-strong rounded mb-4"' +
+        'style="width:100%"'+
+      '/>';
+    });
+    return content;
 }
 
 $('.carousel').carousel({

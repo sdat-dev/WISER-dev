abcc6a09a39c46abbb5613ee704146dfc27ddb43
Sahil Ghauri <sghauri@albany.edu>
2022-04-18 13:39:24 -0400

    updated researchareas page

--- js/researchareas.js
+++ js/researchareas.js
@@ -1,25 +1,38 @@
 let filename = "researchareas";
-let requestURL = "data/researchareas.json";
-let request = new XMLHttpRequest();
-//getting content Element to append grants information
-let maincontentContainer = document.getElementsByClassName('main-content')[0];
-request.open('GET', requestURL);
-request.responseType = 'json';
-request.send();
-request.onload = function () {
-    let content = '';
-    const webelementsjson = request.response;
-    //condition for checking if browser is Internet Explorer
-    let webelements = ((false || !!document.documentMode)) ? JSON.parse(webelementsjson) : webelementsjson;
-    let contentElement = document.createElement('div');
-    contentElement.classList.add('content');
-    contentElement.innerHTML = getContent(webelements,filename);
-    maincontentContainer.appendChild(contentElement);
-    addfooter();
+window.onload = function () {
+    let requestURL = "data/researchareas.json";
+    let datarequestURL = "data/projectslist.json";
+    let request = axios.get(requestURL);
+    let datarequest = axios.get(datarequestURL);
+    let scrollloc = window.location.href.split("#")[1];
+    let maincontentContainer = document.getElementsByClassName('main-content')[0];
+
+    axios.all([request, datarequest]).then(axios.spread((...responses) => {
+        let researchareacontent = responses[0].data;
+        let projects = responses[1].data;
+        let webelements = researchareacontent;
+        let content = getContent(webelements);
+        content += '<div>' + '<ul style="font-size: smaller;font-weight: 600;">' + buildProjectsList(projects) + '</ul>' + '</div>';
+        let contentElement = document.createElement('div');
+        contentElement.classList.add('content');
+        contentElement.innerHTML = content.trim();
+        maincontentContainer.appendChild(contentElement);
+        addfooter();
+        var currentUrl = window.location.href;
+        var divID = currentUrl.split("#", 2);
+        //document.getElementById(divID[1].toString()).scrollIntoView(true); Need to figure it out
+        let scrollelm = document.getElementById(scrollloc);
+        if (scrollloc != null)
+            scrollelm.scrollIntoView({ block: "center" });
+    })).catch(errors => {
+        console.log(errors);
+    })
 }
 
-$('.carousel').carousel({
-    pause: "false",
-    interval: 2000
-
-});
+let buildProjectsList = function(projectslist) {
+    content = '';
+    projectslist.forEach(project => {
+        content += '<li><a href="/projects/' + project.Number + '"> ' + project.Title + '</a></li>'
+    });
+    return content;
+}

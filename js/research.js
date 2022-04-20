926d70e593e538bdbf32305efc386beca3e9a2e5
Sahil Ghauri <sghauri@albany.edu>
2022-04-20 10:39:35 -0400

    corrected issues

--- js/research.js
+++ js/research.js
@@ -1,30 +1,3 @@
-<<<<<<< HEAD:js/researchareas.js
-let filename = "researchareas";
-let requestURL = "data/researchareas.json";
-let request = new XMLHttpRequest();
-//getting content Element to append grants information
-let maincontentContainer = document.getElementsByClassName('main-content')[0];
-request.open('GET', requestURL);
-request.responseType = 'json';
-request.send();
-request.onload = function () {
-    let content = '';
-    const webelementsjson = request.response;
-    //condition for checking if browser is Internet Explorer
-    let webelements = ((false || !!document.documentMode)) ? JSON.parse(webelementsjson) : webelementsjson;
-    let contentElement = document.createElement('div');
-    contentElement.classList.add('content');
-    contentElement.innerHTML = getContent(webelements,filename);
-    maincontentContainer.appendChild(contentElement);
-    addfooter();
-}
-
-$('.carousel').carousel({
-    pause: "false",
-    interval: 2000
-
-});
-=======
 let filename = "research";
 window.onload = function () {
     let requestURL = "data/research.json";
@@ -62,4 +35,3 @@ let buildProjectsList = function(projectslist,shortUrl) {
     });
     return content;
 }
->>>>>>> a7765838987e2b9bfefaa7ce0c7b3dc553d50c2a:js/research.js
